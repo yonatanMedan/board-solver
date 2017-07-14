60e9e03b4ec27bc7371791bd1f86db05212da242
Yonatan Medan <yonatan.medan@gmail.com>
2017-07-14 21:14:30 +0300

    maibe working

--- main/solve2dArrayRoute.js
+++ main/solve2dArrayRoute.js
@@ -1,4 +1,48 @@
 function solve2DArrayRoute($scope,$timeout){
+  function solveArrayRec(array,row,col){
+
+    if(!valid(array,row,col)){
+      return false;
+    }
+    var tile = array[row][col];
+    if(tile.end){
+      tile.route = true;
+      return true;
+    }
+
+    tile.route = true;
+
+    var wait = async().then(function() {
+      return solveArrayRec(array,row+1,col);
+    }).then(function(done){
+      if(!done){
+        return solveArrayRec(array,row,col+1);
+      }else{
+        return done;
+      }
+    }).then(function(done){
+      if(!done){
+        return solveArrayRec(array,row,col-1);
+      }else{
+        return done;
+      }
+    }).then(function(done){
+      if(!done){
+        done = solveArrayRec(array,row-1,col);
+      }else{
+        return done;
+      }
+    }).then(function(done){
+      if(!done){
+        tile.passed = true;
+        tile.route = false;
+      }
+      return done;
+    });
+    return wait;
+
+
+  }
   return{
     solve:function(array,start){
       var startRow = start.row;
@@ -8,9 +52,17 @@ function solve2DArrayRoute($scope,$timeout){
       var done = solveArrayRec(array,startRow,startCol);
       return done;
     }
+
   };
 
 
+}
+function async() {
+  return new Promise(function(resolve) {
+      setTimeout(function(){
+        resolve();
+      }, 100);
+  });
 }
 function valid(array,row,col){
   console.log("row"+row+" "+"col "+col);
@@ -29,34 +81,4 @@ function valid(array,row,col){
   }
   console.log(false);
   return false;
-}
-function solveArrayRec(array,row,col){
-
-  if(!valid(array,row,col)){
-    return false;
-  }
-  var tile = array[row][col];
-  if(tile.end){
-    tile.route = true;
-    return true;
-  }
-
-  tile.route = true;
-
-  var done = solveArrayRec(array,row+1,col);
-  if(!done){
-    done = solveArrayRec(array,row,col+1);
-  }
-  if(!done){
-    done = solveArrayRec(array,row,col-1);
-  }
-  if(!done){
-    done = solveArrayRec(array,row-1,col);
-    console.log(done);
-  }
-  if(!done){
-    tile.passed = true;
-    tile.route = false;
-  }
-  return done;
 }
